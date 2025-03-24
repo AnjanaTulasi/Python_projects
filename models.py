65e9b49c2a74415ebee740b251fb8eac6a527047
AnjanaTulasi <124960528+AnjanaTulasi@users.noreply.github.com>
2025-03-23 22:15:17 -0500

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from django.db import models
-class PatientsLogin(models.Model):
+class PatientsLogin(models.Model): 
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
 class DoctorsLogin(models.Model):
